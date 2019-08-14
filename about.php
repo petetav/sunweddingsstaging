e9d72baaa6e3828e84f981c943c3718d8f93ad12
Peter Tavener <mbp2018@Peters-MBP.lan>
2019-08-14 22:05:29 +0100

    new website test test2 test3 photos added

--- about.php
+++ about.php
@@ -14,7 +14,21 @@
 <!--blurb-->	
 					
 
-	<h2 class="page-title">About us</h2>
+	<h2 class="page-title">My approach</h2>
+	
+	
+<p>Communication is key for me!<br>
+<p>I want you to feel like no question is too small. Planning a wedding is hard enough, but even harder from another country. I understand this and want you to feel confident that you can rely on me to ensure all your questions are answered. I am also aware that ideas pop into your heads morning noon and night! so always feel you can pop me a message or a call as they do and we can create your dream day together.</p>
+<p>Honesty</p><br>
+<p>I am a completely honest wedding planner. I provide full transparency to all my couples. There is absolutely no hidden costs at the end of your wedding. You have full control over their budget, you can contact the vendors that I book for you directly and you pay these vendors directly unless we have agreed otherwise for your convenience.
+Attention to detail.</p>
+<p>I work closely with you to create your vision for your wedding day. No detail is too small. I work on designing events for all shapes and sizes! I work with you to create the wedding you want, one that reflects your style, within your budget and is a representation of you as a couple. Everything is tailored to your wants and needs.</p>
+<p>Friendly and Approachable</p>
+<p>We will spend so much time working together, talking, emailing and meeting while creating your dream wedding day. I want you to know that I appreciate that your wedding is one of the most important days of your lives. I want you to have confidence in me, that I will ensure everything runs smoothly, that you are comfortable enough to be able to ask me anything so you do not feel any stress at all in the build up to your wedding and the day itself.</p>
+<p>Finally I am completely "calm under pressure" so everyone else stays calm and soaks in, appreciates and enjoys every detail of your wedding day.</p>
+	
+	
+	
 	
 <section class="about">	
 	
