6a76e832f41ef697bab5113ad480931520335f9f
Your Name <petethepilot69@gmail.com>
2019-09-03 09:18:13 +0100

    Hamburger menu added and template revised

---
+++ hambuger_menu.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en-GB">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Welcome to Sun Weddings Portugal</title>
+	<meta name="description" content="Wedding planning is our business and we've got a load of experience at it. If you want a professional Irish businesswoman, I'm here for you :) ">
+	
+
+<link href="http://fonts.googleapis.com/css?family=Droid+Serif" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" type="text/css" href="css/hamburger_menu.css">
+</head>
+<!--blurb-->	
+
+<header class="header">
+  <a href="/" class="logo">AGT</a>
+  <input class="menu-btn" type="checkbox" id="menu-btn" />
+  <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+  <ul class="menu">
+    <li><a href="#work">Our Work</a></li>
+    <li><a href="#about">About</a></li>
+    <li><a href="#careers">Careers</a></li>
+    <li><a href="#contact">Contact</a></li>
+  </ul>
+</header>
+
+<main class="main">
+  <div class="section">
+    <a href="#">
+      <span>Moving into a new place</span>
+      <img src="http://media.interiordesign.net.s3.amazonaws.com/West-Elm-Workspace-1-Mid-Century.jpg" alt="" />
+    </a>
+  </div>
+  <div class="section section-split">
+    <a href="#">
+      <span>Music kickstarters</span>
+      <img src="http://kingofwallpapers.com/urban/urban-010.jpg" alt="" />
+    </a>
+    <a href="#">
+      <span>Tag your sound</span>
+      <img src="https://craigmod.com/images/journal/photography_hello/nagano_light.jpg" alt="" />
+    </a>
+  </div>
+  <div class="section section-split">
+    <a href="#">
+      <span>Motels fighting back</span>
+      <img src="http://www.publicdomainpictures.net/pictures/30000/velka/beauty-of-nature-16.jpg" alt="" />
+    </a>
+    <a href="#">
+      <span>Future car design</span>
+      <img src="http://cdn.bgr.com/2016/03/tesla-model-s-p90d.jpg?quality=98&strip=all" alt="" />
+    </a>
+  </div>
+  <div class="section section-split">
+    <a href="#">
+      <span>Following the game</span>
+      <img src="http://www.iberiasoccer.net/images/body-bg.jpg" alt="" />
+    </a>
+    <a href="#">
+      <span>I'm bored</span>
+      <img src="https://c3.eb-cdn.com.au/website/videos/images/screenshots/202879_screenshot_04_l.jpg" alt="" />
+    </a>
+  </div>
+</main>
+
+
+<!-- end of blurb -->
+ <?php include("includes/footer.php"); ?> 
+
+</body>
+</html>
